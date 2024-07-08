185d52c36617ae4d0f0c2242f0c6f119477bcec8
0xgrah4m <pryncetwo@proton.me>
2024-07-08 14:48:13 +0000

    Update BruteForce_DNS.sh

--- Bash-Scripts-Recon/BruteForce_DNS.sh
+++ Bash-Scripts-Recon/BruteForce_DNS.sh
@@ -4,3 +4,11 @@ domain="$1"
 for dns in $(cat wordlist.txt); do
     host $dns.$domain | grep -v "NXDOMAIN"
 done
+
+_______________ Reverse brute force DNS - Resolve address _______________
+#!/bin/bash
+
+ip="37.59.174"
+for n in $(seq 224 239); do
+    host -t PTR $ip.$n | grep -v "37-59-174" | cut -d " " -f5
+done
